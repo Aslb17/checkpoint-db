8b3ad1796dd08d4180d5b037d75f59c08aac237f
Aslb <anne-sophie.leber@socgen.com>
2023-05-23 16:03:19 +0200

    create tables

--- queries/0_create_database.sql
+++ queries/0_create_database.sql
@@ -1,3 +1,39 @@
-# Suit les instruction du checkpoint, en utilisant https://www.dbdesigner.net/, créer un script
-# d'initialisation de BDD SQLite et copie colle son contenu ici
-# (tu dois notmalement avoir +/- 4 instructions CREATE TABLE, chaque instructions doit bien se terminer par un ";")
+CREATE TABLE recruiter (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	login varchar,
+	password varchar,
+	company_id int
+);
+
+CREATE TABLE candidate (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	login varchar,
+	password varchar,
+	name varchar,
+	surname varchar,
+	email varchar,
+	phone_number varchar,
+	presentation text
+);
+
+CREATE TABLE company (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name varchar,
+	decription text
+);
+
+CREATE TABLE offer (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	title varchar,
+	job_decription text,
+	city varchar,
+	company_id int
+);
+
+CREATE TABLE offer_answers (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	offer_id int,
+	candidate_id int
+);
+
+
--- queries/1_insert_data.sql
+++ queries/1_insert_data.sql
@@ -1,3 +1,44 @@
-# Écris dans ce fichier quelques instructions SQL pour insérer de données dans ta BDD
-# N'oublie pas, les requêtes SQL doivent se terminer avec un ";"
-# Pense à céer au moins une entreprise nommée "WCS", à créer des offre d'emploi à "Paris" dont une qui s'intitule "Dev"
+-- Insertion dans la table recruiter
+INSERT INTO recruiter (login, password, company_id)
+VALUES ('johnsmith', 'password1', 1);
+
+INSERT INTO recruiter (login, password, company_id)
+VALUES ('sarahjones', 'password2', 2);
+
+INSERT INTO recruiter (login, password, company_id)
+VALUES ('davidbrown', 'password3', 1);
+
+
+-- Insertion dans la table candidate
+INSERT INTO candidate (login, password, name, surname, email, phone_number, presentation)
+VALUES ('johndoe', 'password1', 'John', 'Doe', 'john.doe@example.com', '1234567890', 'I have 5 years of experience in software development.');
+
+INSERT INTO candidate (login, password, name, surname, email, phone_number, presentation)
+VALUES ('janedoe', 'password2', 'Jane', 'Doe', 'jane.doe@example.com', '9876543210', 'I am a detail-oriented professional with strong communication skills.');
+
+
+-- Insertion dans la table company
+INSERT INTO company (name, description)
+VALUES ('ABC Corporation', 'A leading technology company specializing in software development.');
+
+INSERT INTO company (name, description)
+VALUES ('XYZ Industries', 'A global manufacturing company with a focus on sustainable practices.');
+
+
+-- Insertion dans la table offer
+INSERT INTO offer (title, job_description, city, company_id)
+VALUES ('Software Engineer', 'We are hiring a software engineer with experience in Java and Python.', 'New York', 1);
+
+INSERT INTO offer (id, title, job_description, city, company_id)
+VALUES ('Sales Representative', 'Join our sales team and help us expand our customer base.', 'London', 2);
+
+
+-- Insertion dans la table offer_answers
+INSERT INTO offer_answers (offer_id, candidate_id)
+VALUES (1, 1);
+
+INSERT INTO offer_answers (id, offer_id, candidate_id)
+VALUES (1, 2);
+
+INSERT INTO offer_answers (id, offer_id, candidate_id)
+VALUES (2, 2);
